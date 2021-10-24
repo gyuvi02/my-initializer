30024d7fcc38d710a43adcaaf111c62bbb22b114
gyuvi02 <Gyulus99>
2021-10-24 10:17:31 +0200

    Initial commit

---
+++ my-initializer/src/main/java/org/gyula/myinitializer/MyInitializerApplication.java
@@ -0,0 +1,14 @@
+package org.gyula.myinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyInitializerApplication.class, args);
+	}
+
+
+}
---
+++ my-initializer/src/test/java/org/gyula/myinitializer/MyInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gyula.myinitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
